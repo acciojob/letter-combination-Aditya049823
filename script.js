33e496b2d6ffeeae8426be7990c2422250a0e003
Aditya Harde <110459473+Aditya049823@users.noreply.github.com>
2023-06-15 11:02:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function letterCombinations(input_digit) {
   //Complete the function
-}
+	let table={
+		"2":"abc",
+		"3":"def",
+		"4":"ghi",
+		"5":"jkl",
+		"6":"mno",
+		"7":"pqrs",
+		"8":"tuv",
+		"9":"wxyz"
+	}
+	let ans=[];
 
+	function solve(currIdx,calStr) {
+		if(input_digit.length<=currIdx){
+			ans.push(calStr);
+			return;
+		}
+		let currChar=input_digit[currIdx];
+		for(let i=0;i<table[currChar].length;i++)
+			{
+				solve(currIdx+1,calStr+table[currChar][i]);
+			}
+	}
+	solve(0, "")
+	return ans;
+}
 module.exports = letterCombinations;
